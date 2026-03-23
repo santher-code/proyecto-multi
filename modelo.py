3d221274702d629b6867f58270a11da9f067a83c
santiago h <kunriejeke@gmail.com>
2026-03-22 20:23:55 -0500

    Add receta and usuario classes with initial attributes

---
+++ modelo.py
@@ -0,0 +1,14 @@
+class receta:
+    def __init__(self, nombrerc,ingredientes,cantidad):
+        self.nombre = nombrerc
+        self.ingredientes = ingredientes 
+        self.cantidad =cantidad
+        
+   
+class usuario:
+    def __init__(self, nombreu, edad, correo):
+        self.nombre = nombreu
+        self.edad = edad
+        self.correo = correo  
+        self.peso= 0
+        self.contraseña = contraseña
